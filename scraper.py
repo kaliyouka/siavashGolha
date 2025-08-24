09516508def37781f20eadd98e88b0237d023d5b
Siavash Hakim <65317750+kaliyouka@users.noreply.github.com>
2025-08-24 13:56:46 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,8 +6,12 @@ import time
 
 
 def get_mp3_url(golha_url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+       headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "https://www.golha.co.uk/", 
+        "Connection": "keep-alive"
     }
     try:
         response = requests.get(golha_url, headers=headers, timeout=10)
@@ -28,3 +32,4 @@ def get_mp3_url(golha_url):
         return None, "No MP3 found on this page. The site may have changed or the URL is incorrect."
     except requests.exceptions.RequestException as e :
         return None , f"Error Fetching Page : {str(e)}"
+
