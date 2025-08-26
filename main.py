b01f1fe97d70e0fb5314a6cb8df88a4abffe6fd2
Siavash Hakim <65317750+kaliyouka@users.noreply.github.com>
2025-08-26 11:01:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -24,5 +24,6 @@ def index():
 app.register_blueprint(downloader_bp)
 
 if __name__ == "__main__":
-    app.run( host='0.0.0.0', port=5000,debug=True)
+    app.run( host='0.0.0.0', port=8000,debug=True)
+
 
